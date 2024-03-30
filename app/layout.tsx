4e5cf5a9bd7ec52749c24ac8f891764c24165f19
Alan Daniel <stylesshjs@gmail.com>
2024-03-30 11:58:02 -0400

    feat: adding analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -43,6 +44,8 @@ export default function RootLayout({
         )}
       >
         {children}
+
+        <Analytics />
       </body>
     </html>
   );
