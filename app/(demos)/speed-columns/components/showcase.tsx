20a62221d35e3ac63696a907269b057d955af99b
Alan Daniel <stylesshjs@gmail.com>
2024-04-02 17:13:24 -0400

    nits on new demo

--- app/(demos)/speed-columns/components/showcase.tsx
+++ app/(demos)/speed-columns/components/showcase.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import { getRandomGradient } from "@/lib/utils";
+import { motion } from "framer-motion";
 
 export function Showcase() {
   return (
@@ -15,11 +18,43 @@ export function Showcase() {
       </article>
 
       <article className="my-20">
-        <div className="w-full aspect-[4/3] bg-orange-500 relative rounded-md overflow-hidden">
-          <h3 className="text-[8vw] text-background font-black absolute -bottom-[4vw] left-1/2 -translate-x-1/2">
+        <motion.div
+          className="w-full aspect-[4/3] bg-foreground/5 relative rounded-md overflow-hidden"
+          animate={{
+            background: "rgb(249 115 22)",
+            transition: {
+              duration: 0.6,
+              delay: 0.7,
+              type: "spring",
+              bounce: 0.4,
+            },
+          }}
+          whileHover={{
+            scale: 1.086,
+            transition: {
+              duration: 0.4,
+              delay: 0,
+              type: "spring",
+              bounce: 0.4,
+            },
+          }}
+        >
+          <motion.h3
+            className="text-[8vw] text-background font-black absolute -bottom-[4vw] left-1/2 -translate-x-1/2"
+            initial={{ opacity: 0, filter: "blur(4px)", marginBottom: -5 }}
+            animate={{
+              opacity: 1,
+              filter: "blur(0px)",
+              marginBottom: 0,
+            }}
+            transition={{
+              duration: 0.2,
+              delay: 1,
+            }}
+          >
             Motion
-          </h3>
-        </div>
+          </motion.h3>
+        </motion.div>
 
         <p className="mt-6">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
@@ -30,21 +65,21 @@ export function Showcase() {
         </p>
       </article>
 
-      <article className="mt-12">
+      <article className="mt-16">
         <h2 className="text-primary text-xl">Gallery</h2>
         <p className="mt-4">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
           facilisi. Donec nec ipsum nec nisl lacinia.
         </p>
 
-        <ul className="flex flex-col gap-y-4 mt-6">
+        <ul className="flex flex-col gap-y-4 mt-20">
           {new Array(6).fill(null).map((_, i) => {
             const [from, to] = getRandomGradient();
 
             return (
               <div
                 key={i}
-                className="aspect-[4/3] bg-gradient-to-br from-black to-background"
+                className="aspect-[4/3] bg-gradient-to-br from-black to-background rounded-md"
                 style={{
                   background: `linear-gradient(135deg, ${from}, ${to})`,
                 }}
