4cbed5449c70f382cc4e58c2519ce8ae513a9cba
Alan Daniel <stylesshjs@gmail.com>
2025-04-26 09:38:11 -0400

    restore utils func

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,23 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+let gradientCount = 0;
+
+export const getRandomGradient = () => {
+  const colors = [
+    ["#99f2c8", "#1f4037"],
+    ["#fc4a1a", "#f7b733"],
+    ["#ff4b1f", "#1fddff"],
+    ["#7F00FF", "#E100FF"],
+    ["#ee0979", "#ff6a00"],
+  ];
+
+  return colors[gradientCount++ % colors.length];
+};
+
+export const timeout = (ms: number) =>
+  new Promise((resolve) => setTimeout(resolve, ms));
